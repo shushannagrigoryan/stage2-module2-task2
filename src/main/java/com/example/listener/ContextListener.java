f766d5450d79d8587dc1fc468dffe673ba623ec0
Shushanna Grigoryan <shush.grig.github@gmail.com>
2023-02-07 22:11:09 +0400

    2

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -2,8 +2,9 @@ package com.example.listener;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import java.time.LocalDateTime;
-
+@WebListener
 public class ContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent e){
